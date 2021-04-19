dd9ccccce0c410c53dac78e0493ca7a43ad2f9b3
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-04-19 11:50:51 +0200

    add some folder

--- src/service/indexTraining.js
+++ src/service/indexTraining.js
@@ -6,54 +6,71 @@ import * as basicLightbox from 'basiclightbox';
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-// import { alert, defaultModules } from '@pnotify/core';
-// import * as PNotifyMobile from '@pnotify/mobile';
-// import { template } from 'handlebars';
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+import { template } from 'handlebars';
 
 const refs = {
-  form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.search-form'),
   load: document.querySelector('.load'),
-  clear: document.querySelector('.clear'),
   toStart: document.querySelector('.toStart'),
+  clear: document.querySelector('.clear'),
 };
 
 const requestParams = {
   query: '',
   page: 1,
 };
+defaultModules.set(PNotifyMobile, {});
 
 function createImage(e) {
   e.preventDefault();
+  requestParams.page = 1;
+  refs.gallery.innerHTML = '';
   const query = e.target.children[0].value;
   requestParams.query = query;
+  if (requestParams.query === '') {
+    error({
+      text: 'enter somesthing',
+      delay: 1000,
+    });
+    return;
+  }
+  refs.load.classList.add('is-open');
+  refs.toStart.classList.add('is-open');
+  refs.clear.classList.add('is-open');
   sendRequest(requestParams.query, requestParams.page).then(data => {
+    if (data.length === 0) {
+      error({
+        text: 'not found',
+        delay: 1000,
+      });
+      refs.load.classList.remove('is-open');
+      refs.toStart.classList.remove('is-open');
+      refs.clear.classList.remove('is-open');
+    }
     refs.gallery.insertAdjacentHTML(
       'beforeend',
-      data.map(el => createMarkup(el)),
+      data.map(el => createMarkup(el)).join(''),
     );
   });
-  refs.load.classList.add('is-open');
-  refs.clear.classList.add('is-open');
-  refs.toStart.classList.add('is-open');
 }
 
 function modalOpen(e) {
-  if (e.target.nodeName === 'IMG') {
-    const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
     <img src=${e.target.dataset.source} width="800" height="600">
 `);
 
-    instance.show();
-  }
+  instance.show();
 }
-
-function loadMore() {
+// window.scrollTo(0, 1000);
+function loadPage() {
   requestParams.page += 1;
   sendRequest(requestParams.query, requestParams.page).then(data => {
     refs.gallery.insertAdjacentHTML(
       'beforeend',
-      data.map(el => createMarkup(el)),
+      data.map(el => createMarkup(el)).join(''),
     );
     const totalScrollHeight = refs.gallery.clientHeight;
     window.scrollTo({
@@ -61,14 +78,6 @@ function loadMore() {
       behavior: 'smooth',
     });
   });
-}
-
-function clearAll(e) {
-  e.preventDefault();
-  refs.gallery.innerHTML = '';
-  refs.load.classList.remove('is-open');
-  refs.clear.classList.remove('is-open');
-  refs.toStart.classList.remove('is-open');
 }
 
 function toTop() {
@@ -79,8 +88,16 @@ function toTop() {
   });
 }
 
-refs.toStart.addEventListener('click', toTop);
+function clearAll(e) {
+  e.preventDefault();
+  refs.load.classList.remove('is-open');
+  refs.toStart.classList.remove('is-open');
+  refs.clear.classList.remove('is-open');
+  refs.gallery.innerHTML = '';
+}
+
 refs.clear.addEventListener('click', clearAll);
-refs.load.addEventListener('click', loadMore);
+refs.toStart.addEventListener('click', toTop);
 refs.gallery.addEventListener('click', modalOpen);
+refs.load.addEventListener('click', loadPage);
 refs.form.addEventListener('submit', createImage);
